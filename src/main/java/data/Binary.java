22980ba8cbced1b88192b84762fed7e36e3195a8
sun <sun18701689022@gmail.com>
2017-08-24 17:29:39 +0800

    imp BinarySingleVector

---
+++ src/main/java/data/Binary.java
@@ -0,0 +1,22 @@
+package data;
+
+/**
+ * Created by Sunny on 17/8/24.
+ */
+public class Binary {
+
+
+    /**
+     *  其他方法介绍 http://www.cnblogs.com/graphics/archive/2010/06/21/1752421.html
+     *  计算 int value 二进制中1的个数
+     * @param source
+     * @return
+     */
+    //TODO 参数兼容
+    public static int count1(int source){
+        int c =0 ; // 计数器
+        for (c =0; source > 0; source >>=1) // 循环移位
+            c += source &1 ; // 如果当前位是1，则计数器加1
+        return c ;
+    }
+}
--- src/main/java/distance/BinarySingleVector.java
+++ src/main/java/distance/BinarySingleVector.java
@@ -1,10 +1,11 @@
 package distance;
 
+import data.Binary;
 import exception.DistanceException;
 
 /**
  * Created by Sunny on 17/8/24.
- *
+ * <p>
  * 这个类 用来解决 https://leetcode.com/problems/hamming-distance/description/ 问题
  */
 public class BinarySingleVector extends AbstractSingleVector<Integer> {
@@ -21,20 +22,27 @@ public class BinarySingleVector extends AbstractSingleVector<Integer> {
                 throw new DistanceException("the 2 objects to compute distance are one null at least");
             } else {
                 //the right code to compute the hamming-distance with binary code
+                //here found that two Integer ^ equal to Integer parseTo BinaryCode and ^
+                //beside we don't need to worry about the leck of high position code for some value:default zero
                 //TODO
+                Integer compareValue = ((BinarySingleVector) vectorizable).getValue();
+                int temp = valueHolder ^ compareValue;
+                //than the aim si to how many 1 in temp's binary code
+                result = Binary.count1(temp);
             }
         } else {
-            throw new DistanceException(String.format("can not compute BinarySingleVector and s% type's distance",vectorizable.getClass().getTypeName()));
+            throw new DistanceException(String.format("can not compute BinarySingleVector and s% type's distance", vectorizable.getClass().getTypeName()));
         }
         return result;
     }
 
     /**
      * 这里 返回值 允许不一致 恰好可以 让 valueHolder变得优雅
+     *
      * @return
      */
     @Override
     public Integer getValue() {
-        return null;
+        return valueHolder;
     }
 }
