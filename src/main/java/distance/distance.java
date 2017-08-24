d02299053389492dd2939c9ad7be5f6ab8f767be
sunny@egenie.cn <sunny@egenie.cn>
2017-08-24 15:21:00 +0800

    create

---
+++ src/main/java/distance/distance.java
@@ -0,0 +1,11 @@
+package distance;
+
+/**
+ * Created by Sunny on 17/8/24.
+ *
+ * 起源:        https://leetcode.com/problems/hamming-distance/description/
+ * 相关概念:    汉明距离(hamming-distance)
+ * 一般化:      矢量距离
+ */
+public class distance {
+}
